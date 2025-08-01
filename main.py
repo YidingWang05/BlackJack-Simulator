689438d227f8758ef2495fb076defdb1797b5427
Yiding Wang <yxw1265@student.bham.ac.uk>
2025-08-01 21:10:56 +0100

    start-up job

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("test")
+chips = 0
+decks = 6 # classic casino numbers of decks for blackjack, but in later update, enable changes to match different styles
+isTraditionalShoe = True #this is used to track if this game use a traditional card shoe.
